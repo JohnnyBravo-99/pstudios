b823fc6ff0b2b1ed33d29e44876497bf1039b58c
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2025-06-30 08:28:29 -0400

    0.2 Landing page
    I have updated the colors to match pstudios color schema. A few animations were added to enliven the page. Need to update the home button..

--- pstudios-landingpage/src/App.js
+++ pstudios-landingpage/src/App.js
@@ -1,41 +1,50 @@
 import logo from './lp_logo.svg';
 import './App.css';
 import React, { useState } from "react";
+import BackgroundVideo from './components/BackgroundVideo';
 
 
 function App() {
   
   const [showImage, setShowImage] = useState(true);
+  const [isVideoActive, setIsVideoActive] = useState(false);
 
   const handleImageClick = () => {
     setShowImage(prev => !prev);
+    setIsVideoActive(prev => !prev);
   };
 
   return (
 
       
-        <div className="Lp-logo-container">
+        <div className="Lp-logo-container" tabIndex={-1}>
           {showImage ? (
-            <div className="App" onClick={handleImageClick}>
+            <div className="App" onClick={handleImageClick} tabIndex={-1}>
 
-              <img src={logo} className="App-logo" alt="logo" />
+              <img src={logo} 
+
+              className="App-logo" 
+              alt="logo" 
+              tabIndex={-1} 
+              draggable={false}
+              reload="auto"
+              playsInLine
+              disabledPictureInPicture
+              controls={false}
+              />
 
             </div>
           ) : (
 
             <div className="Lp-logo-container" >
 
-              <div className="contact-info" onClick={handleImageClick}>
-                <h1>
-                  <p>
-                      Test writing...
-                  </p>
-                </h1>
-              </div>
+              <div className="backBtn" onClick={handleImageClick}> Home </div>
+
+              <BackgroundVideo isVideoActive={isVideoActive}/>
 
             </div>
 
-          )};
+          )}
         </div>
   );
 }
